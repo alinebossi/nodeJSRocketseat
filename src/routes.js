b00969dae77e20db7d5ec14d3cee16e3af6ae537
Aline Bossi <Aline@MacBook-Pro-de-Aline.local>
2019-03-02 16:52:32 -0300

    Adding project to git

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = express.Router();
+const productsController = require('./controllers/ProductController');
+
+routes.get('/products', productsController.index);
+routes.get('/products/:id', productsController.show);
+routes.post('/products', productsController.store);
+routes.put('/products/:id', productsController.update);
+routes.delete('/products/:id', productsController.destroy);
+
+module.exports = routes;
